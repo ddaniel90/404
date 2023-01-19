6e7a8941d84fa80a455b3114ef916cdefde87043
ddaniel90 <ddaniel.andrei@outlook.com>
2023-01-19 16:59:17 +0200

    add some features like js menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+// Menu show desktop
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
+}
+// Menu Hidden desktop
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	});
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// Menu remove mobile
 
+const navLink = document.querySelectorAll('.nav__link');
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu');
+	navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n > n.addEventListener('click', linkAction));
